b3437897a647b431108ad7ba9df59658e33ef236
Jordi Laguarta Soler <jordi.laguarta.soler@gmail.com>
2023-09-16 19:13:20 +0200

    Update combine.py

--- nn/layers/combine.py
+++ nn/layers/combine.py
@@ -1,20 +1,16 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-
 from nn.layers.conv2Dnormed import Conv2DNormed
 from nn.layers.scale import UpSample
 
+class combine_layers(nn.Module):
+    def __init__(self, _nfilters, _norm_type='BatchNorm', norm_groups=None, **kwargs):
+        super(combine_layers, self).__init__()
 
-class combine_layers(torch.nn.Module):
-    def __init__(self, _nfilters, _norm_type='BatchNorm', norm_groups=None, **kwards):
-        super().__init__()
-
-        with self.name_scope():
-            # This performs convolution, no BatchNormalization. No need for bias.
-            self.up = UpSample(_nfilters, _norm_type=_norm_type, norm_groups=norm_groups)
-
-            self.conv_normed = Conv2DNormed(channels=_nfilters, kernel_size=(1, 1), padding=(0, 0),
-                                            _norm_type=_norm_type, norm_groups=norm_groups)
+        self.up = UpSample(_nfilters, _norm_type=_norm_type, norm_groups=norm_groups)
+        self.conv_normed = Conv2DNormed(channels=_nfilters, kernel_size=(1, 1), padding=(0, 0),
+                                        _norm_type=_norm_type, norm_groups=norm_groups)
 
     def forward(self, _layer_lo, _layer_hi):
         up = self.up(_layer_lo)
