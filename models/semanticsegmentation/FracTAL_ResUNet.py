e83efb5a703f470298b590fc07ba418055c6c74d
Emiel Veersma <e_veersma@hotmail.com>
2023-09-16 17:09:56 +0200

    Update FracTAL_ResUNet.py
    Replaced with pytorch code

--- models/semanticsegmentation/FracTAL_ResUNet.py
+++ models/semanticsegmentation/FracTAL_ResUNet.py
@@ -6,17 +6,15 @@ from models.semanticsegmentation.FracTAL_ResUNet_features import FracTAL_ResUNet
 
 class FracTAL_ResUNet_cmtsk(torch.nn.Module):
     def __init__(self, nfilters_init, depth, NClasses, widths=[1], psp_depth=4, verbose=True, norm_type='BatchNorm',
-                 norm_groups=None, nheads_start=8, upFuse=False, ftdepth=5, **kwards):
-        super().__init__(**kwards)
+                 norm_groups=None, nheads_start=8, upFuse=False, ftdepth=5, **kwargs):
+        super().__init__(**kwargs)
 
-        with self.name_scope():
-            self.features = FracTAL_ResUNet_features(nfilters_init=nfilters_init, depth=depth, widths=widths,
-                                                     psp_depth=psp_depth, verbose=verbose, norm_type=norm_type,
-                                                     norm_groups=norm_groups, nheads_start=nheads_start, upFuse=upFuse,
-                                                     ftdepth=ftdepth, **kwards)
-            self.head = Head_CMTSK_BC(nfilters_init, NClasses, norm_type=norm_type, norm_groups=norm_groups, **kwards)
+        self.features = FracTAL_ResUNet_features(nfilters_init=nfilters_init, depth=depth, widths=widths,
+                                                 psp_depth=psp_depth, verbose=verbose, norm_type=norm_type,
+                                                 norm_groups=norm_groups, nheads_start=nheads_start, upFuse=upFuse,
+                                                 ftdepth=ftdepth, **kwargs)
+        self.head = Head_CMTSK_BC(nfilters_init, NClasses, norm_type=norm_type, norm_groups=norm_groups, **kwargs)
 
-    def hybrid_forward(self, F, input):
+    def forward(self, input):
         out1, out2 = self.features(input)
-
         return self.head(out1, out2)
